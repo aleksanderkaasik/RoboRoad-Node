22f67a75ca8d0ada5174ad1793d21a4bce8d730e
Aleksander Kaasik <aleks.kaasik@outlook.com>
2025-09-07 06:37:10 +0300

    Added to node store to server

--- installation.sh
+++ installation.sh
@@ -1,3 +1,17 @@
 for item in $(cat package.txt); do
     apt install -y $item
 done
+
+$domain=$1
+
+URL="http://$domain/api/add"
+
+$data="{ 
+        \"NodeName\": \"$2\",
+        \"NodeAddress\": \"$3\"
+    }"
+
+curl -X POST $URL \
+     -H "Accept: application/json" \
+     -H "Content-Type: application/json" \
+     -d "$data"
